dba5c96e7ab15f20c15088cad1a14010d8300c00
Borisevich34 <borisevich_pavel@bk.ru>
2018-02-22 23:14:39 +0300

    added NotificationServiceExtension

---
+++ NotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,41 @@
+//
+//  NotificationService.swift
+//  NotificationServiceExtension
+//
+//  Created by Pavel on 2/22/18.
+//  Copyright © 2018 itrex. All rights reserved.
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+    
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+    
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            if let stringUrl = bestAttemptContent.userInfo["gif"] as? String, let url = URL(string: stringUrl) {
+                AttachmentDownloader.downloadAttachment(url: url, extension: "gif", completion: { (path, error) in
+                    if let path = path {
+                        let attachment = try! UNNotificationAttachment(identifier: "gif", url: path, options: nil)
+                        bestAttemptContent.attachments = [attachment]                        
+                    }
+                })
+            }
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+}
+
